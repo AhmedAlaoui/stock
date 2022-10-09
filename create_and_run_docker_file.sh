77fa6e76e504dd62d3acd2525f4d15e7d32efd6a
AhmedAlaoui <el.a.my.ahmed@gmail.com>
2022-10-09 16:44:15 -0400

    my-project-ycng-228-363013-b46cd4c4412c json

--- create_and_run_docker_file.sh
+++ create_and_run_docker_file.sh
@@ -2,6 +2,6 @@
 docker build . -f Dockerfile -t my_image
 
 #to run the docker image:
-docker run -p 8080:8080 -v $GOOGLE_APPLICATION_CREDENTIALS:/creds.json -e GOOGLE_APPLICATION_CREDENTIALS=/creds.json my_image
+docker run -p 8080:8080 -v $GOOGLE_APPLICATION_CREDENTIALS:/my-project-ycng-228-363013-b46cd4c4412c.json -e GOOGLE_APPLICATION_CREDENTIALS=/my-project-ycng-228-363013-b46cd4c4412c.json my_image
 #to log into the docker image:
-docker run -it -p 8080:8080 -v $GOOGLE_APPLICATION_CREDENTIALS:/creds.json -e GOOGLE_APPLICATION_CREDENTIALS=/creds.json my_image /bin/bash
+docker run -it -p 8080:8080 -v $GOOGLE_APPLICATION_CREDENTIALS:/my-project-ycng-228-363013-b46cd4c4412c.json -e GOOGLE_APPLICATION_CREDENTIALS=/my-project-ycng-228-363013-b46cd4c4412c.json my_image /bin/bash
