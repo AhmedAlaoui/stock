454c3ce375f981d0be5350c4e4b04cd42bf08b6d
AhmedAlaoui <el.a.my.ahmed@gmail.com>
2022-11-12 16:25:37 -0500

    credential + create model every time

--- src/business_logic/process_query.py
+++ src/business_logic/process_query.py
@@ -31,13 +31,13 @@ class BusinessLogic:
     def _get_or_create_model(self, ticker):
         log = logging.getLogger()
         model_filename = self.get_model_filename_from_ticker(ticker)
-        model = get_model_from_bucket(model_filename, self.get_bucket_name())
-        if model is None:
-            log.warning(f'training model for {ticker}')
-            model = self._model_creator.fit(ticker)
-            with open(model_filename, 'wb') as f:
-                joblib.dump(model, f)
-            upload_file_to_bucket(model_filename, self.get_bucket_name())
+        #model = get_model_from_bucket(model_filename, self.get_bucket_name())
+        #if model is None:
+        log.warning(f'training model for {ticker}')
+        model = self._model_creator.fit(ticker)
+        #    with open(model_filename, 'wb') as f:
+        #        joblib.dump(model, f)
+        #    upload_file_to_bucket(model_filename, self.get_bucket_name())
         return model
 
     def get_model_filename_from_ticker(self, ticker):
